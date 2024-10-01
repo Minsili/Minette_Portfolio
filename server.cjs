3012267ba4db06fc41182d2a315613faae7fcb70
Minsili <dianeminou2@gmail.com>
2024-10-01 07:36:55 +0100

    CSS updated

--- server.cjs
+++ server.cjs
@@ -49,7 +49,7 @@ app.post("/send-email", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
