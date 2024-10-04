7ae080c1c64882b8076b55a9db82ccf57cbb207a
Minsili <dianeminou2@gmail.com>
2024-10-04 06:40:30 +0100

    Update the url in contact.jsx file

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     try {
       console.log("Form data being sent:", formData); // Log form data
   
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://minette-portfolio-jnda.onrender.com//send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
